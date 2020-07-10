c9eed9b1681349de297a3113fbac9c96e6032bd9
Miika <miikakm@protonmail.com>
2020-07-10 21:30:24 +0300

    inlined ChartDataService, TODO search, themes

--- src/main/java/com/javaquarium/views/app/chart/ChartPresenter.java
+++ src/main/java/com/javaquarium/views/app/chart/ChartPresenter.java
@@ -1,10 +1,15 @@
 package com.javaquarium.views.app.chart;
 
+import com.firework.AfterInjection;
 import com.firework.Dependency;
+import com.firework.OnEvent;
 import com.firework.Service;
+import com.javaquarium.Events;
 import com.javaquarium.backend.services.AquariumService;
-import com.javaquarium.backend.services.ChartDataService;
+import com.javaquarium.backend.services.SettingsService;
 import com.javaquarium.backend.services.ThemeService;
+import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,7 +19,12 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static javafx.collections.FXCollections.observableArrayList;
 
 @Service
 public class ChartPresenter implements Initializable {
@@ -27,14 +37,27 @@ public class ChartPresenter implements Initializable {
     @FXML private Button plusFoodBtn;
     @FXML private Button minusFoodBtn;
 
-    @Dependency private ChartDataService chartDataService;
-    @Dependency private AquariumService aquariumService;
+    private final Timer timer = new Timer(true);
+    private final ObservableList<XYChart.Data<String, Number>> paddingData1 = observableArrayList();
+    private final ObservableList<XYChart.Data<String, Number>> paddingData2 = observableArrayList();
+    private final ObservableList<XYChart.Data<String, Number>> oxygenData = observableArrayList();
+    private final ObservableList<XYChart.Data<String, Number>> foodData = observableArrayList();
+    private final ObservableList<XYChart.Data<String, Number>> topData = observableArrayList();
+    private final ObservableList<XYChart.Data<String, Number>> bottomData = observableArrayList();
+    private final ObservableList<String> categories = observableArrayList();
+    private TimerTask timerTask = null;
+    private int points;
+    private double pointsPerSecond;
+    private ReadOnlyDoubleProperty oxygen;
+    private ReadOnlyDoubleProperty food;
+
     @Dependency private ThemeService themeService;
+    @Dependency private AquariumService aquariumService;
 
     @Override public void initialize(URL url, ResourceBundle rb) {
-        categoryAxis.setCategories(chartDataService.getCategories());
+        categoryAxis.setCategories(getCategories());
 
-        for (ObservableList<XYChart.Data<String, Number>> data : chartDataService.getData()) {
+        for (ObservableList<XYChart.Data<String, Number>> data :getData()) {
             lineChart.getData().add(new XYChart.Series<>(data));
         }
 
@@ -46,9 +69,7 @@ public class ChartPresenter implements Initializable {
         aquariumService.getFood().addListener((observableValue, number, t1) ->
                 valueChange((double) t1, plusFoodBtn, minusFoodBtn));
 
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            if (!(e instanceof IndexOutOfBoundsException)) e.printStackTrace();
-        });
+        //Thread.setDefaultUncaughtExceptionHandler((t, e) -> {});
     }
 
     private void valueChange(double val, Button plusBtn, Button minusBtn) {
@@ -80,5 +101,85 @@ public class ChartPresenter implements Initializable {
     @FXML
     private void decreaseFood() {
         aquariumService.addFood(-10);
+    }
+
+    @OnEvent(Events.LOGOUT)
+    private void close() {
+        timerTask.cancel();
+        timerTask = null;
+        categories.clear();
+        getData().forEach(List::clear);
+    }
+
+    @OnEvent(Events.CHARTSETTINGCHANGE)
+    private void reset() {
+        close();
+        fillChart();
+        startUpdater();
+    }
+
+    @OnEvent(Events.PRELOAD)
+    private void fillChart() {
+        points = SettingsService.prettyChartPoints + 1;
+        pointsPerSecond = (points - 1) / (double) SettingsService.chartHistory;
+
+        for (int i = 0; i < points; i++) {
+            int indexFromRight = points - i - 1;
+            String indexInSeconds = Double.toString((double) indexFromRight / pointsPerSecond);
+            categories.add(indexInSeconds);
+            oxygenData.add(new XYChart.Data<>(indexInSeconds, aquariumService.getOxygen().get()));
+            foodData.add(new XYChart.Data<>(indexInSeconds, aquariumService.getFood().get()));
+            topData.add(new XYChart.Data<>(indexInSeconds, 150));
+            bottomData.add(new XYChart.Data<>(indexInSeconds, 50));
+        }
+        paddingData1.add(new XYChart.Data<>("range", 200));
+    }
+
+    @OnEvent(Events.LOGIN)
+    private void startUpdater() {
+        assert timerTask == null;
+        timerTask = newTimerTask();
+        int updateRateMs = (int) (1000 / pointsPerSecond);
+        if (SettingsService.prettyChartPoints > 0)
+            timer.scheduleAtFixedRate(timerTask, updateRateMs, updateRateMs);
+    }
+
+    private TimerTask newTimerTask() {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> {
+                    for (int i = 0; i < points - 1; i++) {
+                        int reverseIndex = points - i - 1;
+                        float indexInSeconds = (float) reverseIndex / (float) pointsPerSecond;
+                        String toString = Float.toString(indexInSeconds);
+                        for (ObservableList<XYChart.Data<String, Number>> data : List.of(foodData, oxygenData)) {
+                            XYChart.Data<String, Number> pointToBeMoved;
+                            try {
+                                pointToBeMoved = data.get(i + 1);
+                            } catch (IndexOutOfBoundsException e) { return; }
+                            XYChart.Data<String, Number> newPoint = new XYChart.Data<>(toString, pointToBeMoved.getYValue());
+                            data.set(i, newPoint);
+                        }
+                    }
+                    foodData.set(points - 1, new XYChart.Data<>("0.0", food.get()));
+                    oxygenData.set(points - 1, new XYChart.Data<>("0.0", oxygen.get()));
+                });
+            }
+        };
+    }
+
+    public List<ObservableList<XYChart.Data<String, Number>>> getData() {
+        return List.of(paddingData1, paddingData2, oxygenData, foodData, topData, bottomData);
+    }
+
+    public ObservableList<String> getCategories() {
+        return categories;
+    }
+
+    @AfterInjection
+    private void afterInjection() {
+        oxygen = aquariumService.getOxygen();
+        food = aquariumService.getFood();
     }
 }
